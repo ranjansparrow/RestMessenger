4a2420c2f961bd6cf9009d57f625c6429b87e33f
ranjansparrow <ranjansparrow@gmail.com>
2017-04-30 12:22:48 -0700

    Done with the post update and delete

--- src/main/java/org/restmessenger/ranjan/RestMessenger/resources/MyResource.java
+++ src/main/java/org/restmessenger/ranjan/RestMessenger/resources/MyResource.java
@@ -2,7 +2,11 @@ package org.restmessenger.ranjan.RestMessenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,6 +26,31 @@ public class MyResource {
 	
 	public List<Message> getMessages(){
 		return ms.getAllMessage();
+	}
+	
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return ms.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id,Message message){
+		message.setId(id);
+		return ms.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long id){
+		ms.removeMessage(id);
 	}
 	
 	@GET
